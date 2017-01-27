ce1c079aa2bff9e4c0d40980626b8c9eee1a0fab
kimberly embry <kimberlyembry@kimberlys-MacBook-Air.local>
2017-01-27 15:01:15 -0800

    Add code for auth and password encryption to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,24 @@ class User < ActiveRecord::Base
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
   validates :password_hash, presence: true
+
+
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password)
+    valid_user = User.find_by(email: email)
+    valid_user.password == password ? valid_user : nil
+  end
+
+
+
+
+
 end
