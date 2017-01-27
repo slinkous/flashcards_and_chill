4a39faeef62ce536adf7869fa8607a5b3ea51d03
kimberly embry <kimberlyembry@kimberlys-MacBook-Air.local>
2017-01-27 15:15:58 -0800

    add routes to direct to new user registration and user profile

--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -1,3 +1,17 @@
 get '/users/new' do
   erb :'users/new'
 end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    redirect :"/users/#{@user.id}"
+  else
+    erb :'users/new' #error handling?
+  end
+end
+
+get '/users/:id' do
+  @user = User.find_by(id: params[:id])
+  erb :'users/show'
+end
