00ff86198506e1b2b4fb509784a038eb4be8a69a
Jon <jon.bent@me.com>
2017-01-30 13:54:10 -0800

    adds some functions with no logic

--- app/models/round.rb
+++ app/models/round.rb
@@ -3,5 +3,22 @@ class Round < ActiveRecord::Base
   belongs_to :user
   has_many :guesses
 
-  
+  validates :deck_id, presence: true
+
+  def correct
+    if user.answer == answer
+      true
+    else
+      false
+    end
+  end
+
+  def answered
+
+
+  end
+
+  def total
+
+  end
 end
