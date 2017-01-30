34a2fc0971cf428c18e114e333a4eec9fe97d28e
kimberly embry <kimberlyembry@kimberlys-MacBook-Air.local>
2017-01-29 19:34:46 -0800

    Add new deck and associated cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,4 +25,27 @@ Guess.create({
   correct: false,
   round_id: 1,
   card_id: 1
-  })  
+  })
+
+Deck.create({
+  title: "SEX",
+  user_id: 1
+  })
+
+Card.create({
+  question: "How effective is the pull out method (withdrawal)?",
+  answer: "96%"
+  deck_id: 2
+  })
+
+Card.create({
+  question: "How effective is the fertility awareness method (FAM)?",
+  answer: "76%"
+  deck_id: 2
+  })
+
+Card.create({
+  question: "Is chlamydia contractable from oral sex?",
+  answer: "Yes"
+  deck_id: 2
+  })
